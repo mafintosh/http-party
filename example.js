c0424f55fa1dad894faf130860244ab8a29ad609
Mathias Buus <mathiasbuus@gmail.com>
2014-07-28 13:06:36 +0200

    added example

--- example.js
+++ example.js
@@ -1,9 +1,10 @@
 var party = require('./');
 
-party('PORT',
+party(10000,
 	function(server) {
+		console.log('yo im a server now')
 		server.on('request', function(request, response) {
-			response.end('hello world?');
+			response.end('hello world from '+process.pid+'\n');
 		});
 	},
 	function(err, port) {
