4c2d499082a28794777510c6963e9c0442f2fc74
Mathias Buus <mathiasbuus@gmail.com>
2014-07-28 14:33:13 +0200

    updated style and fixed various issues

--- index.js
+++ index.js
@@ -1,117 +1,138 @@
-var fs = require('fs');
-var freeport = require('freeport');
-var http = require('http');
-var once = require('once');
+var fs = require('fs')
+var freeport = require('freeport')
+var http = require('http')
+var once = require('once')
+
+var noop = function() {}
 
 var writePort = function(path, port, cb) {
-	port += '';
-	fs.open(path, 'wx', function(err, fd) {
-		if (err) return cb(err);
-		fs.write(fd, new Buffer(port), 0, port.length, 0, function(err) {
-			if (err) return cb(err);
-			fs.close(fd, cb);
-		});
-	});
-};
+  port += ''
+  fs.open(path, 'wx', function(err, fd) {
+    if (err) return cb(err)
+    fs.write(fd, new Buffer(port), 0, port.length, 0, function(err) {
+      if (err) return cb(err)
+      fs.close(fd, cb)
+    })
+  })
+}
 
 var readPort = function(path, cb) {
   if (typeof path === 'number') return cb(null, path)
-	fs.readFile(path, 'utf-8', function(err, port) {
-		if (port) return cb(null, parseInt(port));
-		freeport(function(err, port) {
-			if (err) return cb(err);
-			writePort(path, port, function() {
-				readPort(path, cb);
-			});
-		});
-	});
-};
+  fs.readFile(path, 'utf-8', function(err, port) {
+    if (port) return cb(null, parseInt(port))
+    freeport(function(err, port) {
+      if (err) return cb(err)
+      writePort(path, port, function() {
+        readPort(path, cb)
+      })
+    })
+  })
+}
 
 var unlink = function(port, cb) {
-  if (typeof port === 'number') return cb();
-  fs.unlink(port, cb);
-};
+  if (typeof port === 'number') return cb()
+  fs.unlink(port, cb)
+}
 
 module.exports = function(path, onserver, ready) {
-	if (typeof path === 'function') return module.exports(null, null, path, opts);
-	if (!path) path = 'PORT';
+  if (typeof path === 'function') return module.exports(null, null, path, opts)
+  if (!path) path = 'PORT'
 
-	var onping = function(response) {
-		setTimeout(function() {
-			response.end();
-		}, 60*1000);
-	};
+  var server = http.createServer()
+  var emit = server.emit
 
-	var server = http.createServer();
-	var emit = server.emit;
+  var onpingrequest = function(req, res) {
+    var end = function() {
+      res.end()
+    }
 
-	server.emit = function(name, request, response) {
-		if (name !== 'request') return emit.apply(server, arguments);
-		if (request.url === '/__test__') return response.end();
-		if (request.url === '/__ping__') return onping(response);
-		return emit.apply(server, arguments);
-	};
+    var onclose = function() {
+      clearTimeout(timeout)
+    }
 
-	ready = once(ready || function() {});
+    var timeout = setTimeout(end, 60*1000)
+    res.on('close', onclose)
+  }
 
-	var kick = function(tries) {
-		readPort(path, function(err, port) {
-			if (err) return ready(err);
+  var ontestrequest = function(req, res) {
+    res.end()
+  }
 
-			var ping = function() {
-				var req = http.get('http://127.0.0.1:'+port+'/__test__', ping);
+  server.emit = function(name, req, res) {
+    if (name !== 'request') return emit.apply(server, arguments)
+    if (req.url === '/__test__') return ontestrequest(req, res)
+    if (req.url === '/__ping__') return onpingrequest(req, res)
+    return emit.apply(server, arguments)
+  }
 
-				req.setTimeout(2*60000, function() {
-					req.destroy();
-				});
+  ready = once(ready || noop)
 
-				req.on('error', function() {
-					setTimeout(function() {
-						kick(0);
-					}, 50);
-				});
-			};
+  var kick = function(tries) {
+    if (!tries) tries = 0
 
-			var clean = function() {
-				server.removeListener('error', onerror);
-				server.removeListener('listening', onlisten);
-			};
+    readPort(path, function(err, port) {
+      if (err) return ready(err)
 
-			var onlisten = function() {
-				clean();
-				onserver(server, port);
-				ready(null, port);
-			};
+      var onpingresponse = function(res) {
+        res.resume()
+        res.on('end', ping)
+      }
 
-			var onerror = function() {
-				if (err && err.code !== 'EADDRINUSE') return;
+      var ping = function() {
+        var req = http.get('http://127.0.0.1:'+port+'/__ping__', onpingresponse)
 
-				clean();
+        req.setTimeout(2*60000, function() {
+          req.destroy()
+        })
 
-				var req = http.get('http://127.0.0.1:'+port+'/__test__', function(response) {
-					ready(null, port);
-					ping();
-				});
+        req.on('error', function() {
+          setTimeout(kick, 50)
+        })
+      }
 
-				req.setTimeout(2000, function() {
-					req.destroy();
-				});
+      var clean = function() {
+        server.removeListener('error', onerror)
+        server.removeListener('listening', onlisten)
+      }
 
-				req.on('error', function() {
-					if (tries < 3) return kick(tries++);
-					unlink(path, function(err) {
-						if (err) return ready(err);
-						kick(0);
-					});
-				});
-			};
+      var onlisten = function() {
+        clean()
+        onserver(server, port)
+        ready(null, port)
+      }
 
-			server.on('error', onerror);
-			server.on('listening', onlisten);
+      var onerror = function() {
+        if (err && err.code !== 'EADDRINUSE') return
 
-			server.listen(port);
-		});
-	};
+        clean()
 
-	kick(0);
-};
+        var req = http.get('http://127.0.0.1:'+port+'/__test__', function(res) {
+          res.resume()
+          res.on('end', function() {
+            ready(null, port)
+            ping()
+          })
+        })
+
+        req.setTimeout(2000, function() {
+          req.destroy()
+        })
+
+        req.on('error', function() {
+          if (tries < 3) return kick(tries++)
+          unlink(path, function(err) {
+            if (err) return ready(err)
+            kick(0)
+          })
+        })
+      }
+
+      server.on('error', onerror)
+      server.on('listening', onlisten)
+
+      server.listen(port)
+    })
+  }
+
+  kick(0)
+}
